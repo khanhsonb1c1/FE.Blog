5db20f00dde49bafd1d5424dbff7db8db944823b
Son <son@khanhson.local>
2023-02-19 15:48:28 +0700

    convert content to html

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,45 +1,45 @@
 <template>
     <div>
-        <p>compoent con</p>
+        <p>{{ content }}</p>
         <div class="c">
-            <!-- <QuillEditor :modules="modules" toolbar="full" :options="options" v-model:content="content"/> -->
+            <QuillEditor v-model:content="content" content-type="html" output="html"/>
 
         </div>
 </div>  </template>
 
 <script>
-// import { QuillEditor } from '@vueup/vue-quill'
-// import BlotFormatter from 'quill-blot-formatter'
-// import '@vueup/vue-quill/dist/vue-quill.snow.css'
+import { QuillEditor } from '@vueup/vue-quill'
+import BlotFormatter from 'quill-blot-formatter'
+import '@vueup/vue-quill/dist/vue-quill.snow.css'
 
 export default {
 
-    // setup: () => {
-    //     const modules = {
-    //         name: 'blotFormatter',
-    //         module: BlotFormatter,
-    //         options: {/* options */ }
-    //     }
-    //     return { modules }
-    // },
-    // components: {
-    //     QuillEditor
-    // },
+    setup: () => {
+        const modules = {
+            name: 'blotFormatter',
+            module: BlotFormatter,
+            options: {/* options */ }
+        }
+        return { modules }
+    },
+    components: {
+        QuillEditor
+    },
     data() {
-        // return {
-        //     options: {
-        //         debug: 'info',
-        //         modules: {
-        //             toolbar: ['bold', 'italic', 'underline']
-        //         },
-        //         placeholder: '...',
-        //         readOnly: false,
-        //         theme: 'snow',
-        //         // return: 'html'
-        //     },
+        return {
+            options: {
+                debug: 'info',
+                modules: {
+                    toolbar: ['bold', 'italic', 'underline']
+                },
+                placeholder: '...',
+                readOnly: false,
+                theme: 'snow',
+                // return: 'html'
+            },
 
-        //     content: {},
-        // }
+            content: '',
+        }
     }
 }
 </script>
