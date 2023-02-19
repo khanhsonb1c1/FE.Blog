481a381596567034f731a2694905b13f15acd761
Son <son@khanhson.local>
2023-02-19 15:34:06 +0700

    first

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <div class="app">
+    <p>day la view</p>
+    <home-page />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import HomePage from "./components/HomePage.vue";
+
+export default defineComponent({
+  components:{HomePage},
+  data() {
+    return {
+
+    };
+  }
+});
+</script>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <p>compoent con</p>
+        <div class="c">
+            <!-- <QuillEditor :modules="modules" toolbar="full" :options="options" v-model:content="content"/> -->
+
+        </div>
+</div>  </template>
+
+<script>
+// import { QuillEditor } from '@vueup/vue-quill'
+// import BlotFormatter from 'quill-blot-formatter'
+// import '@vueup/vue-quill/dist/vue-quill.snow.css'
+
+export default {
+
+    // setup: () => {
+    //     const modules = {
+    //         name: 'blotFormatter',
+    //         module: BlotFormatter,
+    //         options: {/* options */ }
+    //     }
+    //     return { modules }
+    // },
+    // components: {
+    //     QuillEditor
+    // },
+    data() {
+        // return {
+        //     options: {
+        //         debug: 'info',
+        //         modules: {
+        //             toolbar: ['bold', 'italic', 'underline']
+        //         },
+        //         placeholder: '...',
+        //         readOnly: false,
+        //         theme: 'snow',
+        //         // return: 'html'
+        //     },
+
+        //     content: {},
+        // }
+    }
+}
+</script>
+
+<style></style>
